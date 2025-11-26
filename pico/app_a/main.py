8a766eeaed9a4924a7cd0c58d519805b4ff8d339
Andy Bramah <apbramah@gmail.com>
2025-11-26 13:54:37 +0000

    Make app main friendlier

--- pico/app_a/main.py
+++ pico/app_a/main.py
@@ -1,13 +1,19 @@
 import os
 
+ota_present = False
 try:
     import ota
+    ota_present = True
     ota.update()
 except Exception as e:
     print("OTA update failed:", e)
+
+def ota_trust():
+    if ota_present:
+        ota.trust()
 
 from time import sleep
 while True:
     print("App running in directory:", os.getcwd())
     sleep(0.5)
-    ota.trust()
+    ota_trust()
