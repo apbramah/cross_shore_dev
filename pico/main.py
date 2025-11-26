2b8503decb4b8e1492a5f781bc8bd8d11d960f51
Andy Bramah <apbramah@gmail.com>
2025-11-26 15:03:43 +0000

    Simplify main

--- pico/main.py
+++ pico/main.py
@@ -1,43 +1,10 @@
-import sys, os, json
-from ota_update import rollback, get_active_dir, check_for_version_update, trust, reboot
+import sys
+from ota_update import run_active_app, trust
 
 # Create a simple API module for the app
 class OTA_API:
     trust = staticmethod(trust)
 
 sys.modules["ota"] = OTA_API()
-
-def run_active_app():
-    app_dir = get_active_dir()
-    print("Booting app from:", app_dir)
-
-    try:
-        os.chdir(app_dir)
-        try:
-            with open('manifest.json') as f:
-                manifest = json.load(f)
-                print(manifest)
-                if manifest["trusted"]:
-                    print("App is trusted.")
-                else:
-                    print("App is NOT trusted.")
-                    manifest["num_boot_attempts"] += 1
-                    print("Boot attempts:", manifest["num_boot_attempts"])
-
-                    if manifest["num_boot_attempts"] > 3:
-                        print("Too many failed boot attempts. Rolling back.")
-                        rollback()
-
-                    with open('manifest.json', 'w') as f:
-                        json.dump(manifest, f)                    
-        except:
-            pass
-
-        check_for_version_update()
-        import main
-        main.main()
-    except Exception as e:
-        print("App crashed:", e)
-        reboot()
 
 run_active_app()
--- pico/ota_update.py
+++ pico/ota_update.py
@@ -218,3 +218,36 @@ def check_for_version_update():
         check_for_updates()
     except Exception as e:
         print("OTA failed:", e)
+
+def run_active_app():
+    app_dir = get_active_dir()
+    print("Booting app from:", app_dir)
+
+    try:
+        os.chdir(app_dir)
+        try:
+            with open('manifest.json') as f:
+                manifest = json.load(f)
+                print(manifest)
+                if manifest["trusted"]:
+                    print("App is trusted.")
+                else:
+                    print("App is NOT trusted.")
+                    manifest["num_boot_attempts"] += 1
+                    print("Boot attempts:", manifest["num_boot_attempts"])
+
+                    if manifest["num_boot_attempts"] > 3:
+                        print("Too many failed boot attempts. Rolling back.")
+                        rollback()
+
+                    with open('manifest.json', 'w') as f:
+                        json.dump(manifest, f)                    
+        except:
+            pass
+
+        check_for_version_update()
+        import main
+        main.main()
+    except Exception as e:
+        print("App crashed:", e)
+        reboot()
