01a8798b9e890832b510f6f65876fe18d435972e
Andy Bramah <apbramah@gmail.com>
2025-11-27 10:19:33 +0000

    Change port of second http server; Cleanup of ota_update.py

--- pico/main.py
+++ pico/main.py
@@ -1,3 +1,3 @@
 import ota_update
 
-ota_update.run_active_app(['http://192.168.1.52:80'])
+ota_update.run_active_app(['http://192.168.1.52:8000', 'http://192.168.1.52:80'])
--- pico/ota_update.py
+++ pico/ota_update.py
@@ -1,6 +1,4 @@
-import network, sys, os, time, machine, json, hashlib
-import urequests
-wdt = machine.WDT(timeout=8000)   # timeout in milliseconds
+import network, sys, os, time, machine, json, urequests
 
 def path_exists(path):
     try:
@@ -29,11 +27,7 @@ def makedirs(path):
             except OSError:
                 pass  # already exists or race condition
 
-# ------------------------------
-# Wi-Fi
-# ------------------------------
-def connect_wifi():
-    # Setup Ethernet
+def connect_network():
     nic = network.WIZNET5K()
     nic.active(True)
     nic.ifconfig(('192.168.1.51', '255.255.255.0', '192.168.1.1', '8.8.8.8'))
@@ -42,9 +36,6 @@ def connect_wifi():
         pass
     print("Ethernet connected:", nic.ifconfig())
 
-# ------------------------------
-# Utility functions
-# ------------------------------
 def get_active_dir():
     try:
         with open("/active_slot.txt") as f:
@@ -80,15 +71,6 @@ def rollback():
     print("Switched active slot to:", target_dir)
     reboot()
 
-def trust():
-    wdt.feed()
-    with open('manifest.json') as f:
-        manifest = json.load(f)
-        if not manifest["trusted"]:
-            manifest["trusted"] = True
-            with open('manifest.json', 'w') as f:
-                json.dump(manifest, f)
-
 def cleanup_dir(path):
     # if not os.path.exists(path):
     #     return
@@ -116,35 +98,12 @@ def download_file(url, dest_path):
 
     resp.close()
 
-def file_hash(path):
-    h = hashlib.sha256()
-    with open(path, "rb") as f:
-        while True:
-            chunk = f.read(512)
-            if not chunk:
-                break
-            h.update(chunk)
-    return h.hexdigest()
-
-# ------------------------------
-# OTA logic
-# ------------------------------
 def load_manifest(home_url):
     print("Fetching manifest...")
     resp = urequests.get(f"{home_url}/manifest.json")
     manifest = json.loads(resp.text)
     resp.close()
     return manifest
-
-def verify_files(manifest, base_dir):
-    print("Verifying files...")
-    for path, expected in manifest["files"].items():
-        local = f"{base_dir}/{path}"
-        actual = file_hash(local)
-        if actual != expected:
-            print("Hash mismatch:", path)
-            return False
-    return True
 
 def apply_update(home_url, manifest):
     target_dir = get_target_dir()
@@ -170,9 +129,6 @@ def apply_update(home_url, manifest):
         dest = f"{target_dir}/{path}"
         download_file(url, dest)
 
-    # if not verify_files(manifest, target_dir):
-    #     raise RuntimeError("File verification failed")
-
     manifest["num_boot_attempts"] = 0
     manifest["trusted"] = False
     with open(f"{target_dir}/manifest.json", "w") as f:
@@ -197,23 +153,16 @@ def check_for_updates(home_url):
     else:
         print("No update required. Current version:", local_version)
 
-# ------------------------------
-# Main
-# ------------------------------
 def check_for_version_update(home_urls):
     for home_url in home_urls:
         try:
-            connect_wifi()
+            connect_network()
             check_for_updates(home_url)
             break
         except Exception as e:
             print("OTA failed:", e)
 
-# Create a simple API module for the app
-class OTA_API:
-    trust = staticmethod(trust)
-
-sys.modules["ota"] = OTA_API()
+wdt = None
 
 def run_active_app(home_urls):
     app_dir = get_active_dir()
@@ -242,8 +191,35 @@ def run_active_app(home_urls):
             pass
 
         check_for_version_update(home_urls)
+
+        global wdt
+        wdt = machine.WDT(timeout=8000)   # timeout in milliseconds
+
         import main
         main.main()
     except Exception as e:
         print("App crashed:", e)
         reboot()
+
+trusted = False
+
+def trust():
+    global wdt, trusted
+    wdt.feed()
+
+    # We use the trusted flag to avoid flash operations on every call
+    if not trusted:
+        with open('manifest.json') as f:
+            manifest = json.load(f)
+            if not manifest["trusted"]:
+                manifest["trusted"] = True
+                with open('manifest.json', 'w') as f:
+                    json.dump(manifest, f)
+        trusted = True
+
+# Create a simple API module for the app
+class OTA_API:
+    trust = staticmethod(trust)
+
+sys.modules["ota"] = OTA_API()
+
