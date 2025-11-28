db3bba4dc923894af17397aafdbe51f448b1748c
Andy Bramah <apbramah@gmail.com>
2025-11-28 14:44:16 +0000

    Parse ip and port from simple bgc URL

--- test/bgc.bat
+++ test/bgc.bat
@@ -3,7 +3,26 @@
 :: Path to the log file
 set LOGFILE=C:\Users\apbra\Documents\cross_shore_dev\test\bgc_log.txt
 
-echo ---- New Call ---- >> "%LOGFILE%"
-echo %date% %time% >> "%LOGFILE%"
-echo Arguments: %* >> "%LOGFILE%"
-echo. >> "%LOGFILE%"
+setlocal enabledelayedexpansion
+
+:: The argument comes in as %1, e.g.:
+:: simplebgc://192.168.60.218:8080/
+
+set "url=%~1"
+
+:: 1. Remove "simplebgc://"
+set "temp=%url:simplebgc://=%"
+
+:: 2. Remove trailing slash if present
+if "!temp:~-1!"=="/" set "temp=!temp:~0,-1!"
+
+:: Now temp = 192.168.60.218:8080
+
+:: 3. Split at the colon
+for /f "tokens=1,2 delims=:" %%a in ("!temp!") do (
+    set "ip=%%a"
+    set "port=%%b"
+)
+
+echo IP = %ip% >> "%LOGFILE%"
+echo PORT = %port% >> "%LOGFILE%"
