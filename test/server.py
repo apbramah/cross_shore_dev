f974b404991fd4b6daa3f074da87b10ab59bf000
Andy Bramah <apbramah@gmail.com>
2025-12-02 13:51:50 +0000

    Simplify server.py

--- test/server.py
+++ test/server.py
@@ -59,20 +59,9 @@ async def handler(ws):
 
             # If message came from device → broadcast to all browsers
             elif ws in heads:
-                if 'get:' in message:
-                    filename = message.split('get:')[1]
-                    print("Server: Sending file to device:", filename)
-                    f = open(filename, 'r')
-                    data = f.read()
-                    f.close()
-                    await ws.send(data)
-                elif 'log:' in message:
-                    log_msg = message.split('log:')[1]
-                    print("Device:", log_msg)
-                else:
-                    for ctrl in controllers:
-                        print("Server: Forwarding", message, "to browser")
-                        await ctrl.send(message)
+                for ctrl in controllers:
+                    print("Server: Forwarding", message, "to browser")
+                    await ctrl.send(message)
 
     except websockets.ConnectionClosed:
         print("Connection closed")
