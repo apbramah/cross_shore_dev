6738e536a1e3490fbbaf9c0664aa37fb7d114cfa
Andy Bramah <apbramah@gmail.com>
2025-12-15 10:14:09 +0000

    JUst send back to sender

--- apps/base/udp_con.py
+++ apps/base/udp_con.py
@@ -364,13 +364,12 @@ async def perform_udp_hole_punch(peer_ip, peer_port, peer_uid, existing_socket=N
                 response = data.decode('utf-8')
                 print(f"Received message from {addr}: {response}")
                 test_data = f"HOLE_PUNCH_RESPONSE".encode('utf-8')
+                sock.sendto(test_data, addr)
                 if response == 'HOLE_PUNCH':
                     if addr == peer_addr:
                         print(f"Received punch from expected server reflexive address:", addr)
-                        sock.sendto(test_data, peer_addr)
                     else:
                         print(f"Received punch from unexpected address: {addr}")
-                        sock.sendto(test_data, addr)
                         peer_prflx_addr = addr
                 elif response == 'HOLE_PUNCH_RESPONSE':
                     if addr == peer_addr:
