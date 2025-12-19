1c31cc5b42e10a43558fe7ddeea0aa2a44a2fb50
Andy Bramah <apbramah@gmail.com>
2025-12-19 11:49:35 +0000

    CLean up bgc.py

--- apps/head/bgc.py
+++ apps/head/bgc.py
@@ -1,29 +1,22 @@
 from machine import UART
 import struct
 
+UART_ID = 0           # 0 or 1
+UART_BAUD = 115200
+BUFFER_SIZE = 1024
+
+PACKET_START = 0x24
+
+CMD_SET_ADJ_VARS_VAL = 31
+CMD_API_VIRT_CH_CONTROL = 45
+CMD_CONTROL = 67
+CMD_BEEP_SOUND = 89
 
 class BGC:
     """Encapsulate BGC UART communication and related helpers."""
 
-    UART_ID = 0           # 0 or 1
-    UART_BAUD = 115200
-    BUFFER_SIZE = 1024
-
-    PACKET_START = 0x24
-
-    CMD_SET_ADJ_VARS_VAL = 31
-    CMD_API_VIRT_CH_CONTROL = 45
-    CMD_CONTROL_EXT = 121
-    CMD_CONTROL = 67
-    CMD_BEEP_SOUND = 89
-
     def __init__(self):
-        self.uart = UART(self.UART_ID, self.UART_BAUD)
-
-    @staticmethod
-    def hexdump(data: bytes) -> str:
-        """Return a hex dump string for given bytes."""
-        return " ".join(f"{b:02X}" for b in data)
+        self.uart = UART(UART_ID, UART_BAUD)
 
     @staticmethod
     def decode_udp_packet(data: bytes):
@@ -72,21 +65,21 @@ class BGC:
                     crc_register ^= polynomial
         return crc_register
 
-    def create_packet(self, command_id, payload):
+    def send_cmd(self, command_id, payload):
         payload_size = len(payload)
         header_checksum = (command_id + payload_size) % 256
         header = bytearray([command_id, payload_size, header_checksum])
         header_and_payload = header + payload
         crc = self.crc16_calculate(header_and_payload)
         crc_bytes = bytearray([crc & 0xFF, (crc >> 8) & 0xFF])
-        return bytearray([self.PACKET_START]) + header_and_payload + crc_bytes
+        packet = bytearray([PACKET_START]) + header_and_payload + crc_bytes
+        self.uart.write(packet)
 
     # === High-level helpers corresponding to specific CMD_ values ===
 
     def set_gyro_heading_adjustment(self):
         payload = bytearray([0x01, 0x26, 0x00, 0x15, 0x00, 0x00])
-        packet = self.create_packet(self.CMD_SET_ADJ_VARS_VAL, payload)
-        self.uart.write(packet)
+        self.send_cmd(CMD_SET_ADJ_VARS_VAL, payload)
 
     def disable_angle_mode(self):
         payload = bytearray([
@@ -95,19 +88,16 @@ class BGC:
             0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00
         ])
-        packet = self.create_packet(self.CMD_CONTROL, payload)
-        self.uart.write(packet)
+        self.send_cmd(CMD_CONTROL, payload)
 
     def beep(self):
         payload = bytearray([0x02, 0x00, 0x00, 0x00, 0x00, 0x00,
                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
-        packet = self.create_packet(self.CMD_BEEP_SOUND, payload)
-        self.uart.write(packet)
+        self.send_cmd(CMD_BEEP_SOUND, payload)
 
     def send_joystick_control(self, yaw, pitch, roll):
         payload = struct.pack(">3H", yaw, pitch, roll)
-        packet = self.create_packet(self.CMD_API_VIRT_CH_CONTROL, payload)
-        print("UART joystick -->", self.hexdump(packet))
-        self.uart.write(packet)
+        print("UART joystick -->", yaw, pitch, roll)
+        self.send_cmd(CMD_API_VIRT_CH_CONTROL, payload)
 
 
